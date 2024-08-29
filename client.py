162e2f2655e33e230697df22e66803f9685d75dc
uenoshnj <aiueno6@gmail.com>
2024-08-29 17:37:10 +0900

    client serverの修正（ステージ1）完了

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ import socket
 import sys
 import os
 import threading
+import time
 
 class Udp_Client:
     def __init__(self) -> None:
@@ -18,24 +19,30 @@ class Udp_Client:
         self.serverHost: str = '0.0.0.0'
         self.serverPort: int = 9001
         self.clientHost: str = '127.0.0.1'
-        self.clientPort: int = 9050
+        self.clientPort: int = 999999
 
         self.username: str = ''
         self.usernamelen: int = 0
         self.buffer: int = 4096
+        self.lastSendTime: float = 0
 
         self.faultCount: int = 0
 
     # ソケット紐づけ
     def setBind(self) -> None:
         self.sock.bind((self.clientHost, self.clientPort))
+        self.lastSendTime = time.perf_counter()
 
     # データ送信
     def send(self) -> None:
-        while True:
+        while True:    
             message: str = input()
+            if self.checkTimeout():
+                message = 'exit'
+                print('Time out!')
             data: bytes = self.usernamelen.to_bytes(1, 'big') + f'{self.username}{message}'.encode('utf-8')
             self.sock.sendto(data, (self.serverHost, self.serverPort))
+            self.lastSendTime = time.perf_counter()
 
             if message == 'exit':
                 self.sock.close()
@@ -46,38 +53,55 @@ class Udp_Client:
     def receive(self) -> None:
         while True:
             data: bytes = self.sock.recv(self.buffer)
-            message = data.decode('utf-8')
+            usernamelen = int.from_bytes(data[:1], 'big')
+            username = data[1:1+usernamelen].decode('utf-8')
+            message = data[1+usernamelen:].decode('utf-8')
 
-            if message[message.find(':') + 1:] == 'exit':
+            if message == 'exit' and username == self.username:
                 self.sock.close()
                 break
-
-            print(message)
+            elif message == 'exit':
+                print(f'{username} leave chat')
+            else:
+                print(f'{username} : {message}')
 
 
     # 入力の文字数チェック
-    def isValidLength(self, input: str, len: int) -> bool:
+    def isValidLength(self, input: str, length: int) -> bool:
         l: int = len(input)
-        return 0 < l and l <= len
+        return 0 < l and l <= length
 
     # ユーザ名入力、エンコード
-    def inputUsername(self) -> None:
+    def inputUsername(self) -> str:
         while not self.isValidLength(self.username, 10):
-            self.username = input('User name(Up to 10 characters): ')
+            self.username = input('user name(Up to 10 characters): ')
         
-        return self.username.encode('utf-8')
+        return self.username
+    
+    # ポート入力
+    def inputPort(self) -> None:
+        while not self.isValidLength(str(self.clientPort), 5):
+            port = input('port(Up to 5 digits): ')
+            self.clientPort = int(port)
+    
+    # 送信時間経過チェック
+    def checkTimeout(self) -> bool:
+        return time.perf_counter() - self.lastSendTime >= 60
 
     # 接続
     def communicate(self) -> None:
-        print(f'Connected {self.serverAddress}')
+        # ポート入力
+        self.inputPort()
 
         # ソケットとの紐づけ
         self.setBind()
 
-        # ユーザ名入力の呼び出し、エンコード
-        usernameBits = self.inputUsername()
+        # ユーザ名入力
+        usernameBits = self.inputUsername().encode('utf-8')
 
         self.usernamelen = len(usernameBits)
+
+        print("\nChat start!\nif you want to leave, enter 'exit'\n")
 
         # スレッドの作成、実行
         sendThread: threading.Thread = threading.Thread(target=self.send)
@@ -94,154 +118,6 @@ class Udp_Client:
 
 def main():
     udp_client_chat: Udp_Client = Udp_Client()
-    udp_client_chat.setBind()
     udp_client_chat.communicate()
 
-
-# # サーバのアドレス、ポート
-# SERVER_ADDRESS = '0.0.0.0'
-# SERVER_PORT = 9001
-
-# # クライアントのアドレス
-# ADDRESS = ''
-
-# # チャットのタイムアウト時間(秒)
-# TIMEOUT = 30
-
-# # メッセージ送信失敗最大回数
-# MAXSENDCOUNT = 3
-
-# # 接続削除用
-# CLOSECONNECTION = 'exit'
-
-# # ヘッダー情報のフォーマット
-# def protocolHeader(usernameLen, dataLen):
-#     return usernameLen.to_bytes(1) + dataLen.to_bytes(4)
-
-# # UDPソケットの作成
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# print(f'Connected {SERVER_ADDRESS}:{SERVER_PORT}')
-
-# # ユーザ情報作成
-# client = Udp_Client()
-
-# userNameBits = client.getUserName().encode()
-
-# # 接続の紐づけ
-# sock.bind((ADDRESS, client.getPort()))
-
-# print('\nChat start!\n'\
-#     "if you want to leave, Enter 'exit'\n")
-
-# # タイムアウトの設定
-# sock.settimeout(30)
-
-# # 子プロセスの作成
-# pid = os.fork()
-
-# # 子プロセスの場合、メッセージ送信
-# if pid == 0:
-#     try:
-#         while True:
-#             # メッセージの入力
-#             userMessage = input()
-#             userMessageBits = userMessage.encode()
-
-#             # ヘッダの作成
-#             header = protocolHeader(len(userNameBits), len(userMessageBits))
-
-#             # ヘッダの送信
-#             sock.sendto(header, (SERVER_ADDRESS, SERVER_PORT))
-
-#             # ユーザ名の送信
-#             sock.sendto(userNameBits, (SERVER_ADDRESS, SERVER_PORT))
-
-#             if userMessage == CLOSECONNECTION:
-#                 sock.sendto(CLOSECONNECTION.encode(), (SERVER_ADDRESS, SERVER_PORT))
-#                 break
-#             # メッセージの送信
-#             else:
-#                 sock.sendto(userMessageBits, (SERVER_ADDRESS, SERVER_PORT))
-            
-
-#     except TimeoutError as e:
-#         # ヘッダの作成
-#         header = protocolHeader(len(userNameBits), len(userMessageBits))
-
-#         # ヘッダの送信
-#         sock.sendto(header, (SERVER_ADDRESS, SERVER_PORT))
-
-#         # ユーザ名の送信
-#         sock.sendto(userNameBits, (SERVER_ADDRESS, SERVER_PORT))
-#         sock.sendto(CLOSECONNECTION.encode(), (SERVER_ADDRESS, SERVER_PORT))
-#         print(f'error: {e}')
-
-#     except OSError as e:
-#         # ヘッダの作成
-#         header = protocolHeader(len(userNameBits), len(userMessageBits))
-
-#         # ヘッダの送信
-#         sock.sendto(header, (SERVER_ADDRESS, SERVER_PORT))
-
-#         # ユーザ名の送信
-#         sock.sendto(userNameBits, (SERVER_ADDRESS, SERVER_PORT))
-
-#         sock.sendto(CLOSECONNECTION.encode(), (SERVER_ADDRESS, SERVER_PORT))
-#         print(f'error: {e}')
-    
-#     except Exception as e:
-#         # ヘッダの作成
-#         header = protocolHeader(len(userNameBits), len(userMessageBits))
-
-#         # ヘッダの送信
-#         sock.sendto(header, (SERVER_ADDRESS, SERVER_PORT))
-
-#         # ユーザ名の送信
-#         sock.sendto(userNameBits, (SERVER_ADDRESS, SERVER_PORT))
-        
-#         sock.sendto(CLOSECONNECTION.encode(), (SERVER_ADDRESS, SERVER_PORT))
-#         print(f'error: {e}')
-
-#     finally:
-#         print('Disconnect')
-#         sock.close()
-#         sys.exit(1)
-
-# # 親プロセスの場合、メッセージ受信
-# else:
-#     try:
-#         while True:
-#             # メッセージの受信
-#             data, server = sock.recvfrom(4096)
-#             print(data.decode())
-
-#     except OSError as e:
-#         # ヘッダの作成
-#         header = protocolHeader(len(userNameBits), len(CLOSECONNECTION))
-
-#         # ヘッダの送信
-#         sock.sendto(header, (SERVER_ADDRESS, SERVER_PORT))
-
-#         # ユーザ名の送信
-#         sock.sendto(userNameBits, (SERVER_ADDRESS, SERVER_PORT))
-        
-#         sock.sendto(CLOSECONNECTION.encode(), (SERVER_ADDRESS, SERVER_PORT))
-#         print(f'error: {e}')
-    
-#     except Exception as e:
-#         # ヘッダの作成
-#         header = protocolHeader(len(userNameBits), len(CLOSECONNECTION))
-
-#         # ヘッダの送信
-#         sock.sendto(header, (SERVER_ADDRESS, SERVER_PORT))
-
-#         # ユーザ名の送信
-#         sock.sendto(userNameBits, (SERVER_ADDRESS, SERVER_PORT))
-        
-#         sock.sendto(CLOSECONNECTION.encode(), (SERVER_ADDRESS, SERVER_PORT))
-#         print(f'error: {e}')
-    
-#     finally:
-#         print('Disconnect')
-#         sock.close()
-#         sys.exit(1)
+main()
--- server.py
+++ server.py
@@ -12,6 +12,7 @@ import os
 import socket
 import sys
 import time
+import threading
 
 class Udp_Server:
     def __init__(self) -> None:
@@ -20,118 +21,59 @@ class Udp_Server:
         self.serverPort: int = 9001
 
         self.buffer: int = 4096
-        self.user_dict: dict[str, list] = {}
+        self.user_dict: dict[str, tuple] = {}
 
     # ソケット紐づけ
     def setBind(self) -> None:
         self.sock.bind((self.serverHost, self.serverPort))
+        print('Waiting for receive data.')
     
     # データ送信
-    def send(self, sendData: bytes) -> None:        
-        for address in self.user_dict.values() :
+    def send(self, sendData: bytes) -> None:
+        for address in self.user_dict.values():
             self.sock.sendto(sendData, address)
 
     # データ受信
     def receive(self) -> bytes:
-        # 型ヒント
-        recvdata: bytes
-        address: tuple[str, int]
+        return  self.sock.recvfrom(self.buffer)
 
-        # データ受信
-        recvdata, address = self.sock.recvfrom(4096)
+    # ユーザの存在確認
+    def isExistUser(self, username: str) -> bool:
+        return username in self.user_dict.keys()
 
-        # バイトから文字列に変換
-        usernamelen: int = int.from_bytes(recvdata[:1], 'big')
-        username: str = recvdata[1:usernamelen].decode('utf-8')
-        msg: str = recvdata[1 + usernamelen:].decode('utf-8')
-        time_start: float = time.perf_counter()
-        self.user_dict[username] = [address, time_start]
-        
-
-        return recvdata
-
-    # リレーシステムからユーザを削除
-    def deleteTimeOverUser(self, username: str) -> None:
-        for user in self.user_dict.keys():
-            if time.perf_counter() - self.user_dict[user][1] >= 60:
-                del self.user_dict[user]
-    
     def communication(self) -> None:
         self.setBind()
-        receiveData: bytes = self.receive()
+        try:
+            while True:
+                # 型ヒント
+                receiveData: bytes
+                address: tuple
+                
+                receiveData, address = self.receive()
+                
+                # バイトから文字列に変換
+                usernamelen: int = int.from_bytes(receiveData[:1], 'big')
+                username: str = receiveData[1:1 + usernamelen].decode('utf-8')
+                msg: str = receiveData[1 + usernamelen:].decode('utf-8')
+                
+                if not self.isExistUser(username):
+                    self.user_dict[username] = address
 
-        self.deleteTimeOverUser()
+                # タイムアウトユーザ、退出ユーザをリレーシステムから削除
+                if msg == 'exit':
+                    print(f"delete {username} from relay")
+                    del self.user_dict[username]
+
+                self.send(receiveData)
+            
+        finally:
+            self.sock.close()
 
     
 
 def main():
 
+    udp_server_chat: Udp_Server = Udp_Server()
+    udp_server_chat.communication()
 
-
-
-# DPATH = 'temp'
-# MESSAGELOG = 'message.log'
-# SERVER_ADDRESS = '0.0.0.0'
-# SERVER_PORT = 9001
-
-# # メッセージ保存用ディレクトリの作成
-# if not os.path.exists(DPATH):
-#     os.makedirs(DPATH)
-
-# # 接続中のユーザ
-# user_dict = {}
-
-# # UDPソケット作成
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# print(f'starting up on port {SERVER_PORT}')
-
-# # アドレス紐づけ
-# sock.bind((SERVER_ADDRESS, SERVER_PORT))
-
-# try:
-#     while True:
-#         # ヘッダを受信し、ユーザ名の長さとデータの長さを変数に格納
-#         header = sock.recv(5)
-#         usernamelen = int.from_bytes(header[:1])
-#         data_length = int.from_bytes(header[1:])
-
-#         # ユーザ名取得
-#         username = sock.recv(usernamelen).decode()
-
-#         # データ(メッセージ)を受信
-#         data, address = sock.recvfrom(data_length)
-#         message = data.decode()
-
-#         # ユーザ名が既に存在している場合、メッセージを返却。
-#         if username in user_dict.keys() and address[1] not in user_dict.values():
-#             sock.sendto(b'user is already exist!', address)
-#             continue
-
-#         # クライアントからexitを受け取った場合、ユーザ削除
-#         if message == 'exit':
-#             user_dict.pop(username)
-#         # それ以外の場合
-#         else:
-#             # key：ユーザ名, value：アドレスで辞書に格納
-#             if username not in user_dict.keys():
-#                 user_dict[username] = address
-
-#             # ユーザ名：メッセージの形式でファイルに保存
-#             user_message = f'{username}: {message}'
-#             with open(os.path.join(DPATH, MESSAGELOG), 'a') as f:
-#                 f.write(user_message + '\n')
-
-#             # 接続中のすべてのクライアントにメッセージを送信
-#         for value in user_dict.values():
-#             sock.sendto(user_message.encode(), value)
-
-
-# except OSError as e:
-#     print(f'OSerror: {e}')
-
-# except Exception as e:
-#     print(f'Exception: {e}')
-
-# finally:
-#     sock.close()
+main()
